cc157a9828696401cf61936793ae02e1dff12f96
Anshul Sharma <anshuls.cs.20@nitj.ac.in>
2022-02-26 20:06:17 +0530

    specified routes

---
+++ node/app.js
@@ -0,0 +1,41 @@
+
+const express = require("express");
+const path= require("path");
+// console.log(path)
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get('/',function (req,res) {
+    res.sendFile(path.join(__dirname,'index.html'))
+    
+})
+app.get('/home',function (req,res) {
+    res.render('home')
+    
+})
+
+app.get('/login',function (req,res) {
+    res.render('login')
+})
+app.get('/signup',function (req,res) {
+    res.render('signup')
+})
+
+app.get('/farmerdash',function (req,res) {
+    res.render('farmerdash')
+})
+
+app.get('/companydash',function (req,res) {
+    res.render('companydash')
+})
+
+app.listen((3000 || process.env.PORT), function() {
+    console.log("Server started on port 3000");
+  });
+  
